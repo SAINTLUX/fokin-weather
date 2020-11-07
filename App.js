0b4c25a3733855a5594a081283743e883c381651
saintluxpgw <saintluxpgw@gmail.com>
2020-11-07 17:25:49 +0900

    Import Weather Data

--- App.js
+++ App.js
@@ -1,30 +1,34 @@
 import React from "react";
-import {Alert} from "react-native";
+import { Alert } from "react-native";
 import Loading from "./Loading";
-import * as Location from 'expo-location';
+import * as Location from "expo-location";
+import axios from "axios";
 
-
-// export default function App() {
-//   return <Loading />;
-// }
+const API_KEY = "d5f33cf1e8911bd2a6cbcbd3b3798fe9";
 
 export default class extends React.Component {
   state = {
     isLoading: true
-  }
-  getLocation = async() => {
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  };
+  getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
-      const { 
-        coords: { latitude, longitude } 
+      const {
+        coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
-      this.setState({ isLoading: false});
-      // Send to API and get weather!
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
     } catch (error) {
-      Alert.alert("Can`t find your location.", "Try again");
+      Alert.alert("Can't find you.", "So sad");
     }
-  }
-  componentDidMount() { 
+  };
+  componentDidMount() {
     this.getLocation();
   }
   render() {
