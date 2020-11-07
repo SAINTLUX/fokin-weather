407e3fffa14c86afa66be967d295111a11c47f47
saintluxpgw <saintluxpgw@gmail.com>
2020-11-07 16:56:55 +0900

    Setting the Permissions

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {Alert} from "react-native";
+import Loading from "./Loading";
+import * as Location from 'expo-location';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+// export default function App() {
+//   return <Loading />;
+// }
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  }
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();
+      const { 
+        coords: { latitude, longitude } 
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false});
+      // Send to API and get weather!
+    } catch (error) {
+      Alert.alert("Can`t find your location.", "Try again");
+    }
+  }
+  componentDidMount() { 
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
